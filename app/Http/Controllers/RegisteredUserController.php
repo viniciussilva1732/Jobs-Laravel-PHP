3b63cf264e541cd746f8dabb1c88719c7159d8b6
Vinicius Silva <vinicius.silva1732@gmail.com>
2025-09-24 16:08:26 -0300

    Add LogOut

--- app/Http/Controllers/RegisteredUserController.php
+++ app/Http/Controllers/RegisteredUserController.php
@@ -3,6 +3,11 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Validation\Rules\Password;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+
+
 
 class RegisteredUserController extends Controller
 {
@@ -13,6 +18,23 @@ class RegisteredUserController extends Controller
 
     public function store()
     {
-        dd(request()->all());
+        // validate
+        $validatedAttributes = request()->validate([
+            'first_name' => ['required'],
+            'last_name' => ['required'],
+            'email' => ['required', 'email'], 
+            'password' => ['required', Password::min(6), 'confirmed'], // password_confirmation
+        ]);
+
+        // create the user
+        $user = User::create($validatedAttributes);
+
+        // log in
+        Auth::login($user);
+
+        // redirect somewhere
+        return redirect('/jobs');
+
+
     }
 }
--- app/Http/Controllers/SessionController.php
+++ app/Http/Controllers/SessionController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\Request;
+use Illuminate\Validation\ValidationException;
 
 class SessionController extends Controller
 {
@@ -13,6 +15,30 @@ class SessionController extends Controller
 
     public function store()
     {
-        dd(request()->all());
+        // validate
+        $attributes = request()->validate([
+            'email' => ['required', 'email'],
+            'password' => ['required']
+        ]);
+
+        // attempt to login the user
+        if (! Auth::attempt($attributes)){
+            throw ValidationException::withMessages([
+                'email' => 'Sorry, those credentials do not match.'
+            ]);
+        }
+
+        // regenerate the session token
+        request()->session()->regenerate();
+
+        // redirect
+        return redirect('/jobs');
+    }
+
+    public function destroy()
+    {
+        Auth::logout();
+
+        return redirect('/');
     }
 }
